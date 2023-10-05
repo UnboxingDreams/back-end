839631019a5c90bbe58bfbcfad4efd52db52cbfe
psy-choi <chlrltjd5263@naver.com>
2023-10-05 18:43:11 +0900

    change FCM test logic

--- petmourning/views/testview.py
+++ petmourning/views/testview.py
@@ -8,7 +8,8 @@ from app.settings import SECRET_KEY, JWT_ALGO, REDIS
 
 def testview(request):
     dataPayLoad = {
-        "letterId": 1,
+        "type" : "newLetter",
+        "taskNumber": "1",
         "content" : "편지쓰기 테스트예용"
     }
     token = "eSLPrA6tZblqWaha-FGkZn:APA91bF4KAeQte5UDz_GxBUVC4FXUSit97BkzMypTbBXFf6s3z8egz4_D7xigiZG9-gCWQ-l2P9_gWYnhK50qR-KMyWe3diqmkyz4OJDidEFFvhD_FeFEZeGrxg5ZAXTL0b51oD8pXlp"
