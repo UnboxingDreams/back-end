ae4e6f40d4f36cacdd3fd062266ebda4e2636a87
psy-choi <chlrltjd5263@naver.com>
2023-10-07 20:15:08 +0900

    refact <기성 #35> profile api 호출

--- petmourning/views/profileview.py
+++ petmourning/views/profileview.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from petmourning.models import User
 from petmourning.exception import CustomException
 from petmourning.views.authorization import get_userId, get_userName
+import json
 
 
 def findProfiles(request):
@@ -11,12 +12,20 @@ def findProfiles(request):
             userId = get_userId(request)
             user = User.objects.get(userId = userId)
             if user == None:
-                raise CustomException("유저 데이터가 존재하지 않습니다.", status_code=404)
+                raise CustomException("유저 데이터가 존재하지 않습니다.", status_code=403)
+            
+            animalPicture = json.loads(user.animalImgUrl)
 
-            return JsonResponse(user)
+            data = {
+                'nickName' : user.animalName,
+                'thumb' : animalPicture['thumb'],
+                'alarm' : user.alarm
+            }
+
+            return JsonResponse(data, status = 200)
         else:
             raise CustomException("옳바르지 않은 접근 입니다.", status_code=405)
     except CustomException as e:
-        return JsonResponse({'message' : e.message}, status=e.status_code)
+        return JsonResponse({'message' : e.message}, status=e.getStatus)
     except Exception:
         return JsonResponse({'message' : '데이터 베이스의 오류입니다.'}, status = 404)
